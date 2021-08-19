f0df74bc8133a975563c2b135d59d2e1e43220fd
yuka <yoko.ukai@gmail.com>
2021-08-19 14:53:45 +0900

    edit

---
+++ python/test.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+import logging
+
+
+logger = logging.getLogger(u"test")
+
+
+def main():
+    print(u"main print")
+
+
+if __name__ == u"__main__":
+    print(u"hello")
+    print(u"konitiha")
+    main()
+    
